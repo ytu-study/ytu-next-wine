a0a04821abf0c6cf0846142eb1c23ef4c6a39542
Abangpa1ace <dyek72731@gmail.com>
2022-08-21 19:02:34 +0900

    초기세팅 중

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 30000,
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* <Hydrate state={pageProps.dehydratedState}> */}
+      <Component {...pageProps} />
+      {/* </Hydrate> */}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <div>Big Wine - created by Next.js</div>;
+};
+
+export default Home;
