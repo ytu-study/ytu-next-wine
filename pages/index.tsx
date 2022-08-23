493b15d10aa011c38b7741f7c26f99cff082ff0a
ukyi <devcat.js@gmail.com>
2022-08-23 14:24:15 +0900

    ✅ slack commit message test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import Test from "@/components/Test";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import Test from '@/components/Test';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+console.log('###');
 
 const Home: NextPage = () => {
   return (
