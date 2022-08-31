5ee573ef2afbb70eeb9045937403dcbf617a94a2
ukyi <devcat.js@gmail.com>
2022-08-31 22:52:49 +0900

    ♻️ graphql 모듈로 수정 및 타입 추가

--- hooks/apis/useFetchWines.ts
+++ hooks/apis/useFetchWines.ts
@@ -1,6 +1,8 @@
-import { getWines } from "@/graphql";
-import { useQuery } from "react-query";
+import { Graphql } from '@/graphql';
+import GET_WINES from '@/graphql/query/getWines.gql';
+import { QueryGetWinesArgs } from '@/types/graphql';
+import { useQuery } from 'react-query';
 
-export function useFetchWines() {
-  return useQuery('wines', getWines)
+export function useFetchWines(paginationArgs?: QueryGetWinesArgs) {
+  return useQuery('wines', () => Graphql.get(GET_WINES, paginationArgs).then(data => data.getWines));
 }
