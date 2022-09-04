e9e68f9349554e5d9e7baf76275fb68037ba321f
ukyi <devcat.js@gmail.com>
2022-09-04 17:22:00 +0900

    🏷️ 매개변수 타입 수정

--- modules/dehydrate.ts
+++ modules/dehydrate.ts
@@ -4,7 +4,7 @@ import { FetchQueryOptions, QueryFunction } from 'react-query/types/core/types';
 export class Dehydrate {
   static async prefetchQuery<TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(options: FetchQueryOptions<TQueryFnData, TError, TData, TQueryKey>): Promise<DehydratedState>;
   static async prefetchQuery<TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(queryKey: TQueryKey, options?: FetchQueryOptions<TQueryFnData, TError, TData, TQueryKey>): Promise<DehydratedState>;
-  static async prefetchQuery<TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(queryKey: TQueryKey, queryFn?: QueryFunction<TQueryFnData, TQueryKey> | FetchQueryOptions<TQueryFnData, TError, TData, TQueryKey>, options?: FetchQueryOptions<TQueryFnData, TError, TData, TQueryKey>): Promise<DehydratedState>;
+  static async prefetchQuery<TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(queryKey: TQueryKey, queryFn: QueryFunction<TQueryFnData, TQueryKey>, options?: FetchQueryOptions<TQueryFnData, TError, TData, TQueryKey>): Promise<DehydratedState>;
   static async prefetchQuery(...args: unknown[]) {
     const client = new QueryClient();
     await client.prefetchQuery(...args as Parameters<QueryClient['prefetchQuery']>);
