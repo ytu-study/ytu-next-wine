a86ebcb2ef8707c7c6471470b22c2be1e44cfed5
ukyi <devcat.js@gmail.com>
2022-09-04 16:03:03 +0900

    ✨ data 수정

--- pages/query/index.tsx
+++ pages/query/index.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import { useQuery } from 'react-query';
 
 const QueryPage: NextPage = () => {
-  const { data: { getWines }, isLoading } = useQuery(api.WINE, () => api.fetchWines());
+  const { data , isLoading } = useQuery(api.WINES, () => api.fetchWines());
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <ul>
-      {getWines.map(w => <li key={w._id}>{w.name}</li>)}
+      {data.getWines.map(w => <li key={w._id}>{w.name}</li>)}
     </ul>
   );
 };
