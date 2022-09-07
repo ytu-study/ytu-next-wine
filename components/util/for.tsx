b772dffae1cc488bce5b492f6963993e6e146b67
ukyi <devcat.js@gmail.com>
2022-09-08 00:24:36 +0900

    ✨ object 추가 및 example 주석 추가

--- components/util/for.tsx
+++ components/util/for.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-type ForProps<T extends unknown[], U extends T extends (infer R)[] ? R : never> = {
-  each: readonly U[];
-  children(item: U, index: number, array: readonly U[]): unknown;
+type EachObject = { readonly [P: string]: unknown };
+type EachArray = readonly unknown[];
+type Union<T> = T extends EachArray ? (T extends readonly (infer R)[] ? R : never) : T extends EachObject ? Union<Entries<T>> : never;
+
+type ForProps<T extends EachObject | EachArray, U extends Union<T>> = {
+  each: T;
+  children(item: U, index: number, raw: T): ReactNode;
 };
 
-export default function For<T extends unknown[], U extends T extends (infer R)[] ? R : never>({ children, each }: ForProps<T, U>) {
-  return <>{each.map(children)}</>;
+function isEachArray(each): each is EachArray {
+  return Array.isArray(each);
+}
+/**
+ * @example
+ * - Object
+ * const item = { id: 1, name: "item" };
+ * <For each={item}>
+ *   {([key, value]: ["id", 1] | ["name", "item"], index: number, raw: { id: 1, name: "item" }) => (
+ *     <div>{key}: {value}</div>
+ *   )}
+ * </For>
+ *
+ * - Array
+ * const items = [{ id: 1, name: "item1" }, { id: 2, name: "item2" }];
+ * <For each={items}>
+ *   {(item: { id: number, name: string }, index: number, raw: {id: number, name: string}[]) => (
+ *     <div>id: {item.id}</div>
+ *     <div>name: {item.name}</div>
+ *   )}
+ * </For>
+ */
+export default function For<Raw extends EachObject | EachArray, Item extends Union<Raw>>({ children, each }: ForProps<Raw, Item>) {
+  if (isEachArray(each)) return <>{each.map((item, index, raw) => children(item as Item, index, raw as Raw))}</>;
+  return <>{Object.entries(each).map((item, index, raw) => children(item as Item, index, raw as unknown as Raw))}</>;
 }
