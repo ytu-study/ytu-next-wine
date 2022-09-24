ff66f285f676ce25b98374fecf21e54347e0ae64
ukyi <devcat.js@gmail.com>
2022-09-25 01:42:47 +0900

    ✨ key GET_WINES으로 수정

--- pages/query/index.tsx
+++ pages/query/index.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import { useQuery } from 'react-query';
 
 const QueryPage: NextPage = () => {
-  const { data, isLoading } = useQuery(api.WINES, () => api.fetchWines());
+  const { data, isLoading } = useQuery(api.GET_WINES, () => api.fetchWines());
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <ul>
-      {data.getWines.map(w => (
+      {data?.getWines.map(w => (
         <li key={w._id}>{w.name}</li>
       ))}
     </ul>
