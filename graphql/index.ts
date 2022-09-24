b9e4642d099a6587e382053804e673ceb947a04c
ukyi <devcat.js@gmail.com>
2022-09-25 01:37:45 +0900

    🏷️ Documents 타입 추가

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,5 +1,10 @@
+import { QueryType } from '@/modules/graphql';
 import GET_WINE from './query/getWine.gql';
 import GET_WINES from './query/getWines.gql';
-import GET_WINEIDS from './query/getWineIds.gql';
+import GET_WINE_IDS from './query/getWineIds.gql';
+import GET_VIVINO_WINE from './query/getVivinoWine.gql';
+import GET_VIVINO_WINES from './query/getVivinoWines.gql';
+import { RequestDocument } from 'graphql-request';
 
-export { GET_WINE, GET_WINES, GET_WINEIDS };
+type Documents = { [P in keyof QueryType]: RequestDocument };
+export default { GET_WINE, GET_WINES, GET_WINE_IDS, GET_VIVINO_WINE, GET_VIVINO_WINES } as Documents;
