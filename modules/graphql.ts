f826dc75e124b756ca2c1c2e007121e644f33c0a
ukyi <devcat.js@gmail.com>
2022-09-25 01:37:10 +0900

    🏷️ GraphQLClient queryKey 추가 및 Graphql get 메서드 수정 - Graphql.get 첫번째 매개변수에 queryKey를 받아서 key를 이용해 document 세팅 및 리턴 타입 추론되게 수정

--- modules/graphql.ts
+++ modules/graphql.ts
@@ -1,28 +1,42 @@
+import snakeCase from 'lodash/snakeCase';
 import { Query } from '@/types/graphql';
-import { GraphQLClient, RequestDocument, Variables } from 'graphql-request';
+import { GraphQLClient as _GraphQLClient, Variables } from 'graphql-request';
 import * as Dom from 'graphql-request/src/types.dom';
+import { ScreamingSnakeCase } from 'type-fest';
+import documents from '@/graphql';
 
-export class Graphql extends GraphQLClient {
-  private static graphql: Graphql;
+type QueryResponse = Omit<Query, '__typename'>;
+export type QueryType = { readonly [P in keyof QueryResponse as ScreamingSnakeCase<keyof QueryResponse>]: P };
+
+class GraphQLClient extends _GraphQLClient implements QueryType {
+  readonly GET_VIVINO_WINE = 'getVivinoWine';
+  readonly GET_VIVINO_WINES = 'getVivinoWines';
+  readonly GET_WINE = 'getVivinoWine';
+  readonly GET_WINES = 'getVivinoWine';
+  readonly GET_WINE_COUNTRIES = 'getVivinoWine';
+  readonly GET_WINE_COUNTRY = 'getVivinoWine';
+  readonly GET_WINE_FOOD = 'getVivinoWine';
+  readonly GET_WINE_FOODS = 'getVivinoWine';
+  readonly GET_WINE_GRAPE = 'getVivinoWine';
+  readonly GET_WINE_GRAPES = 'getVivinoWine';
+  readonly GET_WINE_IDS = 'getVivinoWine';
+  readonly GET_WINE_TYPE = 'getVivinoWine';
+  readonly GET_WINE_TYPES = 'getVivinoWine';
 
   constructor() {
     super(process.env.NEXT_PUBLIC_API_URL);
   }
+}
 
-  static get<T = Omit<Query, '__typename'>, V = Variables>(
-    document: RequestDocument,
+export class Graphql extends GraphQLClient {
+  get<K extends keyof QueryResponse, T = Pick<QueryResponse, K>, V = Variables>(
+    key: K,
     variables?: V,
     requestHeaders?: Dom.RequestInit['headers'],
   ): Promise<T> {
-    if (!this.graphql) this.graphql = new Graphql();
-    return this.graphql.get(document, variables, requestHeaders);
-  }
-
-  get<T = Omit<Query, '__typename'>, V = Variables>(
-    document: RequestDocument,
-    variables?: V,
-    requestHeaders?: Dom.RequestInit['headers'],
-  ): Promise<T> {
-    return this.request(document, variables, requestHeaders);
+    const type = snakeCase(key).toUpperCase() as keyof typeof documents;
+    const _document = documents[type];
+    return super.request(_document, variables, requestHeaders);
   }
 }
+export const graphql = new Graphql();
